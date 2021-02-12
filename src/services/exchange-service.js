47a2c1ede926e8ae1c296c1d727a4cd218471f06
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-12 10:36:04 -0800

    Add service logic for API call

---
+++ src/services/exchange-service.js
@@ -0,0 +1,14 @@
+export default class ExchangeService {
+  static getExchange(base, target) {
+    return fetchfetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${target}`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return Error(error);
+      });
+  }
+}
