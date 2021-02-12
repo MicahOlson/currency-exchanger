434edc33eb1b82faea467192e33c608dc8ed649e
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-12 14:59:59 -0800

    Add results to grid and style

--- src/main.js
+++ src/main.js
@@ -7,13 +7,14 @@ import ExchangeService from './services/exchange-service.js';
 function clearPage() {
   $('#base-value').val('');
   $('#target-currency').val('');
-  $('.jumbotron').hide();
+  $('#results').hide();
 }
 
 function getResult(response, baseValue) {
   if (response.result === 'success') {
-    const convertedValue = (baseValue * response.conversion_rate).toFixed(2);
-    $('#result').text(`${response.base_code} ${baseValue.toFixed(2)} = ${response.target_code} ${convertedValue}`);
+    const conversion = (baseValue * response.conversion_rate).toFixed(2);
+    $('#base-result').text(`${baseValue.toFixed(2)} ${response.base_code} = `);
+    $('#target-result').text(`${conversion} ${response.target_code}`);
   } else if (response['error-type'] === 'unsupported-code') {
     $('#result').text("Sorry, we don't have your target currency in today's data. Please select a different target and try again.");
   } else if (response.result === 'error') {
@@ -34,6 +35,6 @@ $(document).ready(function() {
       .then(function(response) {
         getResult(response, baseValue);
       });
-    $('.jumbotron').show();
+    $('#results').show();
   });
 });
