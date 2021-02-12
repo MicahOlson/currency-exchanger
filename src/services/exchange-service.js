1b941ecda9692f1dc15dc58269dc76fde9130823
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-12 12:59:11 -0800

    Fix linting warnings in service logic

--- src/services/exchange-service.js
+++ src/services/exchange-service.js
@@ -1,6 +1,6 @@
 export default class ExchangeService {
   static getExchange(base, target) {
-    return fetchfetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${target}`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${target}`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.status);
